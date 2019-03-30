05bf5ba928155ec3d4e90f12c69b281a4137f95d
Saran Wai <saranwai@Darwins-MacBook-Pro.local>
2019-03-29 20:54:03 -0700

    first commit

---
+++ src/components/pages/NotFound.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+export default () => {
+  return (
+    <div>
+      <h1 className="display-4">
+        404 <span className="text-danger">Page Not Found</span>
+      </h1>
+      <p className="lead">Sorry, That Page Not Found</p>
+    </div>
+  );
+};
