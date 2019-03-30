0a7a6dd65208b9028ef5db1ae208a70cb264f4a4
Saran Wai <saranwai@Darwins-MacBook-Pro.local>
2019-03-29 20:57:19 -0700

    Updates

---
+++ precache-manifest.84276ad4ccf944fb65acfbf2ab330f0a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "a25dc548b2853493d42e",
+    "url": "/contact-manager/static/js/runtime~main.b61356a3.js"
+  },
+  {
+    "revision": "569288ee25d4e969c993",
+    "url": "/contact-manager/static/js/main.d11cc680.chunk.js"
+  },
+  {
+    "revision": "2c7d6d7dcf6b18742aa8",
+    "url": "/contact-manager/static/js/2.aca5d75c.chunk.js"
+  },
+  {
+    "revision": "569288ee25d4e969c993",
+    "url": "/contact-manager/static/css/main.11d832ee.chunk.css"
+  },
+  {
+    "revision": "2c7d6d7dcf6b18742aa8",
+    "url": "/contact-manager/static/css/2.99b074d2.chunk.css"
+  },
+  {
+    "revision": "f027b35cf1493b42d01460176230d194",
+    "url": "/contact-manager/index.html"
+  }
+];
